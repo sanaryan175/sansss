ebd0a514f547bc615aba7492a52e71aa1b19fc4e
sanaryan175 <sanskritig1705@gmail.com>
2026-01-02 22:18:05 +0530

    add files

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! this is aryan')
+})
+
+app.get('/home',(req,res)=>{
+    res.send("hey there!! i am learning backend web dev")
+})
+app.get('/aryan',(req,res)=>{
+    res.send("<h1>aryan is mobile addicted!! he always plays games</h1>")
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
